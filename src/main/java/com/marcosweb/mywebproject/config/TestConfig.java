859df6d1a9aa8cd5a717143336d93441cc5633b7
MarcosAndreBueno <www.bueno@yahoo.com>
2023-04-22 02:40:14 -0300

    OrderStatus enum

--- src/main/java/com/marcosweb/mywebproject/config/TestConfig.java
+++ src/main/java/com/marcosweb/mywebproject/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.marcosweb.mywebproject.entities.Order;
 import com.marcosweb.mywebproject.entities.User;
+import com.marcosweb.mywebproject.entities.enums.OrderStatus;
 import com.marcosweb.mywebproject.repositories.OrderRepository;
 import com.marcosweb.mywebproject.repositories.UserRepository;
 
@@ -39,10 +40,10 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		//teste pedidos
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
